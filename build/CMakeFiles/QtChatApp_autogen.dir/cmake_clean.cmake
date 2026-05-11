d6a953eaa6d1f82c50238ef0fd0e6b5f9c0d0cf5
marcgorgy <marc_gorgy@aucegypt.edu>
2026-05-11 16:50:52 +0300

    small bugs fixed

---
+++ build/CMakeFiles/QtChatApp_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtChatApp_autogen"
+  "QtChatApp_autogen/mocs_compilation.cpp"
+  "QtChatApp_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/QtChatApp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
