2f9e16432fdabb8a3c7d2329280e8997f3a6ea57
mohameddev1711-lang <mohameddev1711@aucegypt.edu>
2026-05-11 16:14:48 +0300

    Update main.cpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <memory> // memory management
+#include <boost/asio.hpp>
+#include <nlohmann/json.hpp> // Standard JSON library for C++
+
+using boost::asio::ip::tcp;
+using json = nlohmann::json;
+using boost::asio::awaitable;
+using boost::asio::co_spawn;
+using boost::asio::detached;
+using boost::asio::use_awaitable;
+using boost::asio::as_tuple;
+
 //
 // Created by Mohamed Fadul on 5/11/26.
 //
